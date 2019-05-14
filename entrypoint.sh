a36b03ca5412a411d7840f56494a1988cd4676f2
wegamekinglc <scrappedprince.li@gmail.com>
2019-05-14 11:51:45 +0800

    update port

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=/app
 cd audio_serve
 export FLASK_APP=app
 export FLASK_ENV=development
-export FLASK_RUN_PORT=5000
+export FLASK_RUN_PORT=8128
 export FLASK_RUN_HOST="0.0.0.0"
 flask run
