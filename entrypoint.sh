804f2b234ee7a5e2bad1efea6c538a6df02664b5
wegamekinglc <scrappedprince.li@gmail.com>
2019-05-14 17:35:02 +0800

    using gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 export LANG=C.UTF-8
 export PYTHONPATH=/app
 cd audio_serve
-export FLASK_APP=app
-export FLASK_ENV=development
-export FLASK_RUN_PORT=8128
-export FLASK_RUN_HOST="0.0.0.0"
-flask run
+# export FLASK_APP=app
+# export FLASK_ENV=development
+# export FLASK_RUN_PORT=8128
+# export FLASK_RUN_HOST="0.0.0.0"
+gunicorn -w 4 -b 0.0.0.0:8128 app:app
