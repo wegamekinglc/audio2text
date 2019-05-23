c5e80baacd014ff1c176dd48322b4e5ac49da610
licheng <licheng@sunmi.com>
2019-05-23 13:50:54 +0800

    add worker parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- export LC_ALL=C.UTF-8
+export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export PYTHONPATH=/app
 cd audio_serve
@@ -7,4 +7,4 @@ cd audio_serve
 # export FLASK_ENV=development
 # export FLASK_RUN_PORT=8128
 # export FLASK_RUN_HOST="0.0.0.0"
-gunicorn -w 4 -b 0.0.0.0:8128 app:app
+gunicorn -w $1 -b 0.0.0.0:8128 app:app
